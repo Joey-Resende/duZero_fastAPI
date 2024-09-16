cea75c1d874d9458fcc944e3a71f113bfd9ca0d3
Joey-Resende <givanildo.resende@gmail.com>
2024-09-16 18:55:43 -0300

    Adicionando mais testes

--- duzero_fastapi/app.py
+++ duzero_fastapi/app.py
@@ -1,7 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from duzero_fastapi.database import get_session
+from duzero_fastapi.models import User
 from duzero_fastapi.schemas import (
     Message,
     UserDB,
@@ -20,15 +24,40 @@ def read_root():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
-    database.append(user_with_id)
-    return user_with_id
+def create_user(user: UserSchema, session=Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
+
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+    limit: int = 10, skip: int = 0, session: Session = Depends(get_session)
+):
+    user = session.scalars(select(User).limit(limit).offset(skip))
+    return {'users': user}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
